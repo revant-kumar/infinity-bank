98ca8c48af0d361e02d99c338752f231dfa7dfb9
revant-kumar <revantx1816@gmail.com>
2024-06-30 18:30:47 +0530

    add doughnut chart to the homepage using chart.js

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,29 @@
+import HeaderBox from '@/components/HeaderBox'
+import TotalBalanceBox from '@/components/TotalBalanceBox' 
 import React from 'react'
 
-const page = () => {
+const Home = () => {
+const loggedIn = {firstName: 'Revant'};
   return (
-    <div>
-      Home
-    </div>
+    <section className="home">
+      <div className="home-content">
+        <header className='home-header'>
+            <HeaderBox
+              type="greeting"
+              title="Welcome"
+              user={loggedIn?.firstName || 'Guest'}
+              subtext="Access and manager your account and transactions efficiently."
+            />
+
+            <TotalBalanceBox 
+              accounts={[]}
+              totalBanks = {1}
+              totalCurrentBalance = {1250}
+            />
+        </header>
+      </div>
+    </section>
   )
 }
 
-export default page
+export default Home
